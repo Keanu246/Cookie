c90be07aa6e9c8be229401745481c31128eaee0c
Keanu246 <45147865+Keanu246@users.noreply.github.com>
2019-08-22 15:10:42 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+var cookieCount = 0;
+var cookie = document.getElementByGetElementById("cookie");
+
+cookie.onclick = function() {
+
+}
+
+cookie.onclick = function() {
+	cookieCount++;
+	alert(cookieCount);
+}
